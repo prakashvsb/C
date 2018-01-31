ee8da93256acde39f1373e850d5b790baf2768a5
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-01-31 16:26:29 +0530

    Create positive or not.C

---
+++ positive or not.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter the number",a);
+scanf("%d",&a);
+if(a>0)
+printf("The number is positive");
+else
+printf("The number is negative");
+getch();
+return 0;
+}
