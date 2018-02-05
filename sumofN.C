9e9b3a9c44e56f887dde637ab97f2a2beceec419
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-02-05 16:03:16 +0530

    Create sumofN.C

---
+++ sumofN.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,x=n;
+printf("Enter the numbers\t");
+scanf("%d&n");
+for(i=1;i<=n;i++);
+{
+x=x+i;
+}
+printf("sum of the N numbers is %d,x");
+}
