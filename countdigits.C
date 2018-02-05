9c947706ada1a098567ad4103e5e9529d8f75e86
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-02-05 16:15:11 +0530

    Create countdigits.C

---
+++ countdigits.C
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+count=0;
+printf("Enter any number:");
+scanf("%d",&num);
+while(num!==0)
+{
+count++;
+num 1=0;
+}
+printf("total digits:%d",count);
+getch();
+return o;
+}
